4c416b3f492678f4e8ea540f41efcbd8f9cc44cb
Ashutosh Das <ashutoshdas2004@gmail.com>
2026-02-13 11:17:22 +0530

    Sync

---
+++ day_6/test.py
@@ -0,0 +1,12 @@
+def display(**kwargs): 
+    for i in kwargs.values(): 
+        print(i) 
+display(emp="Kelly", salary=9000)
+
+'''
+matrix = [[1, 2], [3, 4]] 
+for row in matrix: 
+    for item in row: 
+       print(item, end=" ")
+print()
+'''
